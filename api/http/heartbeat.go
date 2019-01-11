db704fd220387cdc33b6a694ebfe56dbfdc59c6c
yongzhi.yang@dmall.com <you@example.com>
2019-01-11 18:56:45 +0800

    heartbeat

--- api/http/heartbeat.go
+++ api/http/heartbeat.go
@@ -1,43 +1,63 @@
 package http
 
 import (
-	"github.com/YangYongZhi/muxy/log"
+	time2 "time"
+	"log"
 	"github.com/gocron"
-	"io/ioutil"
 	"net/http"
+	"github.com/pquerna/ffjson/ffjson"
+	"bytes"
+	"gitlab.dmall.com/arch/datahub-agent/net"
+	"fmt"
+	"os"
 )
 
 const (
-	serverURL   = `http://localhost:13003/stressng/stressors`
+	serverURL   = `http://wonton.dmall.com:13004/wonton/heartbeat`
+	Port        = 13003
 	sendInteval = 30
+	version     = `WONTON-AGENT-0.0.1`
 )
 
 type HeartBeatSender struct {
 }
 
+type HeartBeats struct {
+	Host         string `json:"host"`
+	Instancecode string `json:"instancecode"`
+	Time         int64  `json:"time"`
+	Version      string `json:"version"`
+	Pid          int    `json:"pid"`
+	StartTime    int64  `json:"startTime"`
+}
+
 func (*HeartBeatSender) Start() {
-	gocron.Every(sendInteval).Seconds().Do(sendHeartBeat)
+	startTime := time2.Now().UnixNano() / 1e6
+	hostIp := net.GetOutboundIP().String()
+	instanceCode := fmt.Sprintf("%s:%d", hostIp, Port)
+	pid := os.Getpid()
+	gocron.Every(sendInteval).Seconds().Do(sendHeartBeat, startTime, hostIp, instanceCode, pid)
 
 	// function Start start all the pending jobs
 	<-gocron.Start()
 }
 
-func sendHeartBeat() {
-	response, err := http.Get(serverURL)
+func sendHeartBeat(startTime int64, hostIp string, instanceCode string, pid int) {
+	params := &HeartBeats{Host: hostIp, Instancecode: instanceCode, Time: time2.Now().UnixNano() / 1e6, Version: version, Pid: pid, StartTime: startTime}
+	log.Printf("The heartbeat Object that send to wonton server : %v", params)
+	postParams, err := ffjson.Marshal(params)
 	if err != nil {
-		log.Error(err.Error())
+		log.Print(err.Error())
 		return
-	} else {
-		defer response.Body.Close()
-		content, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			log.Error(err.Error())
-			return
-		} else {
-			log.Debug("The content for a heartbeat:\n%s", string(content))
-		}
 	}
 
-	job, time := gocron.NextRun()
-	log.Debug("Send a heartbeat, next run time at %v, %v", time, job.NextScheduledTime())
+	//log.Printf("Send a heartbeat as String to dmc : \n%v", string(postParams))
+
+	response, err := http.Post(serverURL, "application/json", bytes.NewBuffer(postParams))
+	if err != nil {
+		log.Print(err.Error())
+		return
+	}
+
+	defer response.Body.Close()
 }
